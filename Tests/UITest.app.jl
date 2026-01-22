dbbcc1a7eea6d15d7308eb8c703f46d118a6cb89
ianet <ian@ellison-taylor.com>
2026-01-22 05:09:05 +0000

    ...

--- Tests/UITest.app.jl
+++ Tests/UITest.app.jl
@@ -4,36 +4,55 @@ win = GtkWindow("UITest", 800, 600)
 grid = GtkGrid()
 push!(win,grid)
 
+function big_label_expanded(text)
+    lbl = GtkLabel(text)
+    Gtk4.markup(lbl, "<span size='61440'>$(text)</span>")
+    lbl.hexpand = true
+    lbl.vexpand = true
+    return lbl
+end 
+
 function big_label(text)
     lbl = GtkLabel(text)
     Gtk4.markup(lbl, "<span size='61440'>$(text)</span>")
     return lbl
 end 
 
-function expand_button(text)
+function big_button(text)
     lbl = big_label(text)
     btn = GtkButton(lbl)
-    btn.hexpand = true
-    btn.vexpand = true
+    signal_connect(btn, "clicked") do btn
+        @info "Button '$text' clicked"
+    end
     return btn
 end
 
-b1 = expand_button("One")
-b2 = expand_button("Two")
-b3 = expand_button("Three")
-b4 = expand_button("Four")
+l1 = big_label_expanded("One")
+l2 = big_label_expanded("Two")
+l3 = big_label_expanded("Three")
+b1 = big_button("Four")
+b2 = big_button("Five")
+b3 = big_button("Six")
 
-lp = GtkGestureLongPress()
-push!(b4, lp)
-signal_connect(lp, "pressed") do gesture, x, y
-    @info "Long press detected at coordinates: $x, $y"
-    Gtk4.G_.set_state(gesture, Gtk4.EventSequenceState_CLAIMED)
-end
+# gesture = GtkGestureClick()
+# push!(b4, gesture)
+# signal_connect(gesture, "pressed") do g, n_press, x, y
+#     Gtk4.G_.set_state(gesture, Gtk4.EventSequenceState_CLAIMED)
+#     @info "Button Pressed"
+# end
 
-grid[1,1] = b1
-grid[2,1] = b2
-grid[1,2] = b3
-grid[2,2] = b4  
+# signal_connect(gesture, "released") do g, n_press, x, y
+#     Gtk4.G_.set_state(gesture, Gtk4.EventSequenceState_CLAIMED)
+#     # w = Gtk4.widget(g)
+#     @info "Button Released"
+# end
+
+grid[1,1] = l1
+grid[2,1] = l2
+grid[3,1] = l3
+grid[1,2] = b1
+grid[2,2] = b2
+grid[3,2] = b3
 
 @async Gtk4.GLib.glib_main()
 Gtk4.GLib.waitforsignal(win,:close_request)
