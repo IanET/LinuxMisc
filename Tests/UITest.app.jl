b853fab10b70f900283756bd4d25134c657cde18
ianet <ian@ellison-taylor.com>
2026-01-22 03:46:01 +0000

    ...

--- Tests/UITest.app.jl
+++ Tests/UITest.app.jl
@@ -4,19 +4,24 @@ win = GtkWindow("UITest", 800, 600)
 grid = GtkGrid()
 push!(win,grid)
 
-function big_button(text)
+function big_label(text)
     lbl = GtkLabel(text)
-    btn = GtkButton(lbl)
     Gtk4.markup(lbl, "<span size='61440'>$(text)</span>")
+    return lbl
+end 
+
+function expand_button(text)
+    lbl = big_label(text)
+    btn = GtkButton(lbl)
     btn.hexpand = true
     btn.vexpand = true
     return btn
 end
 
-b1 = big_button("One")
-b2 = big_button("Two")
-b3 = big_button("Three")
-b4 = big_button("Four")
+b1 = expand_button("One")
+b2 = expand_button("Two")
+b3 = expand_button("Three")
+b4 = expand_button("Four")
 
 grid[1,1] = b1
 grid[2,1] = b2
